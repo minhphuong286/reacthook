de9d347beb1e77befd2ea83d8ed6f85182182d45
minhphuong286 <minhphuongbigboss280@gmail.com>
2022-07-02 22:04:32 +0700

    #10

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Nav from './views/Nav';
+import { useState, useRef, useEffect } from 'react';
 
 function App() {
+  let [name, setName] = useState('MP')
+  let [address, setAddress] = useState('')
+  let [todos, setTodo] = useState([
+    { id: '1', title: 'todo1' },
+    { id: '2', title: 'todo2' }
+  ])
+  const handleEventClick = () => {
+    if (!address) {
+      alert('address is empty');
+      return;
+    }
+    let newTodo = { id: '3', title: address };
+    setTodo([...todos, newTodo])
+    setAddress('')
+  }
+  const handleOnchangeInput = (event) => {
+    setAddress(event.target.value)
+  }
+  let inputFocus = useRef(null);
+  useEffect(() => {
+    inputFocus.current.focus();
+  })
   return (
     <div className="App">
+      <Nav />
       <header className="App-header">
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to react-Hooks,  {name}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reactrtert
-        </a>
+        <input type="text" ref={inputFocus} value={address} onChange={(event) => handleOnchangeInput(event)} />
+        <button type="button" onClick={() => handleEventClick()}>Click me</button>
+        {todos.map((todo, index) => {
+          return (
+            <div className='todo-child' key={index}>{todo.id} - {todo.title}</div>
+          )
+        })
+
+        }
       </header>
     </div>
   );
